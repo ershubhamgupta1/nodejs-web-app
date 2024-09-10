41df4a6bce3dd08d31f5ff638b1257cce38cf4f0
shubham gupta <shubhamgupta@shubhams-MacBook-Air.local>
2024-09-10 16:41:27 +0530

    conect sql db

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const {connectToDatabase} = require('./db');
+
 
 // Serve a simple response
 app.get('/', (req, res) => {
@@ -11,4 +13,5 @@ app.get('/', (req, res) => {
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  connectToDatabase();
 });
