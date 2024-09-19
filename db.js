96fd8702f95d15a29eccb74a9cf85dddaa3f2028
Shubham Gupta <ershubhamgupta1@gmail.com>
2024-09-19 16:54:52 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,39 +1,39 @@
-const sql = require('mssql');
-require('dotenv').config(); // Load .env file
+// const sql = require('mssql');
+// require('dotenv').config(); // Load .env file
 
-// Database configuration
-const config = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER,
-    database: process.env.DB_NAME,
-    options: {
-        encrypt: true, // Use encryption
-        trustServerCertificate: false // Set to true if you are using self-signed certificates
-    }
-};
+// // Database configuration
+// const config = {
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     server: process.env.DB_SERVER,
+//     database: process.env.DB_NAME,
+//     options: {
+//         encrypt: true, // Use encryption
+//         trustServerCertificate: false // Set to true if you are using self-signed certificates
+//     }
+// };
 
-// Function to connect to the database
-async function connectToDatabase() {
-  try {
-    // Create a connection pool
-    const pool = await sql.connect(config);
+// // Function to connect to the database
+// async function connectToDatabase() {
+//   try {
+//     // Create a connection pool
+//     const pool = await sql.connect(config);
 
-    console.log('Connected to SQL Database');
+//     console.log('Connected to SQL Database');
 
-    // Use the pool to execute queries, etc.
-    // Example query
-    const result = await pool.request().query('SELECT * FROM Persons');
-    console.log(result.recordset);
+//     // Use the pool to execute queries, etc.
+//     // Example query
+//     const result = await pool.request().query('SELECT * FROM Persons');
+//     console.log(result.recordset);
 
-  } catch (err) {
-    console.error('Database connection failed:', err);
-  } finally {
-    // Close the connection
-    sql.close();
-  }
-}
+//   } catch (err) {
+//     console.error('Database connection failed:', err);
+//   } finally {
+//     // Close the connection
+//     sql.close();
+//   }
+// }
 
-module.exports = {connectToDatabase}
+// module.exports = {connectToDatabase}
 
-// Call the function
+// // Call the function
